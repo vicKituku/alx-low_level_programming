f55e9e8db5168ae24c376afc6525430812cb3070
vicKituku <victorkituku@gmail.com>
2022-12-08 12:27:14 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-/*Entry point of the program
+/*
+ * Entry point of the program
  *returns 0
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
