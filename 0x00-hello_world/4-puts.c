78695b72944a3589aa9d5c9c28307006d09286fd
vicKituku <victorkituku@gmail.com>
2022-12-08 12:57:34 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-/*
- * main: Entry point
- * Description:Uses puts to output a string of characters
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
