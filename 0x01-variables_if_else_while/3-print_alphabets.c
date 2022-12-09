21b641ea2df2d4920be4198dfab8d88347220d95
vicKituku <victorkituku@gmail.com>
2022-12-09 08:10:09 -0800

    Print alpahbets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char character;
+character = 'a';
+while (character <= 'z')
+{
+putchar(character);
+character++;
+}
+character = 'A';
+while (character <= 'Z')
+putchar(character);
+character++;
+}
+putchar('\n');
+return (0);
+}
