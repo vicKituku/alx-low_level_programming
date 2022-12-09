74056354711fc284593e8d451c7a4796c0455db3
vicKituku <victorkituku@gmail.com>
2022-12-09 09:47:09 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
