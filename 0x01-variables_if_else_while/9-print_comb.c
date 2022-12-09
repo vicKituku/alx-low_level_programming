93c2648b6a18b777d20f0c50e287ff0f28b7353b
vicKituku <victorkituku@gmail.com>
2022-12-09 10:38:18 -0800

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = o; i < 10; i++)
+{
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
