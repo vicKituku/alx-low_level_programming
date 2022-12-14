2f5ce0e7b8cd9aa4a1b177ed2e444ebacfa77ffd
vicKituku <victorkituku@gmail.com>
2022-12-14 14:57:41 -0800

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+include "main.h"
+/**
+ * print_last_digit - last digit of a number
+ * @n: int type number
+ * Return: last digit of n
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	if (n < 0)
+	{
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+	}
+}
