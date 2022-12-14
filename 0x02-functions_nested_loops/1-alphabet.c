ca68a82a48f1e816393111e71cc0f3a0ea32c793
vicKituku <victorkituku@gmail.com>
2022-12-14 06:58:32 -0800

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,18 @@
  * main - Entry point
  * Return: 0
  */
+/**
+ * print_alphabet - prints the alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z': ch++)
+{
+	_putchar(ch)
+}
+}
 int main(void)
 {
 print_alphabet();
