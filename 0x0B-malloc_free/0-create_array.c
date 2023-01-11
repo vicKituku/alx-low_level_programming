f47d54e33c09bb550e4588d2c0289f7c13efd18d
vicKituku <victorkituku@gmail.com>
2023-01-10 23:41:19 -0800

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc((size) * sizeof(char));
 	if (size == 0)
 		return (NULL);
 	if (s == NULL)
