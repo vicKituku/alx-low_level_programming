c34ce62017921a55b107776cd57c8bcba58e8049
vicKituku <victorkituku@gmail.com>
2023-01-11 03:14:15 -0800

    str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - Concatates two strings
+ * Return: pointer to newly allocated space of memory
+ * * @s1: string one
+ * @s2: string two
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, k, ph;
+	char *strout;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	strout = malloc(sizeof(char) * ((i + j) + 1));
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		strout[k] = s1[k];
+	}
+	j = ph;
+	for (j = 0; j < ph; j++, k++)
+		strout[k] = s2[j];
+	return (strout);
+}
+
