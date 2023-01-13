0e2c486c45df41c5658d420942dfb792f9662eef
vicKituku <victorkituku@gmail.com>
2023-01-13 02:19:59 -0800

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		m[i] = s1[i];
 	ph = i;
-	for (j = 0; j < (n / sizeof(char); j++, ph++)
+	for (j = 0; j < (n / sizeof(char)); j++, ph++)
 			m[ph] = s2[j];
 	m[ph + 1] = '\0';
 	return (m);
