2d8b873ffae2a8a483ddec056546dcf9fa383ea6
vicKituku <victorkituku@gmail.com>
2023-01-13 02:52:26 -0800

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,18 @@
 #include <stdlib.h>
-
+/**
+ * _calloc - allocates memory for an array
+ * Return: pointer to allocated memory for the array
+ * @nmemb: number of members for the array
+ * @size: size of each member in bytes
+ */
 void *_calloc(unsigned int nmemb, unsigned int size);
 {
+	int *t;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	t = malloc(nmemb * size);
+	if (t == NULL)
+		return (NULL);
+	return (t);
+}
