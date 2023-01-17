3a8c94e4e629d32295bf7576c304e1416eb89518
vicKituku <victorkituku@gmail.com>
2023-01-17 03:03:14 -0800

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+/**
+ * struct dogo - define new type struct dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: owner of dog
+ */
 
 typedef struct dog
 {
