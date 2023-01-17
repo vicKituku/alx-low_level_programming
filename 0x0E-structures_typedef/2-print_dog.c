d7b38ec1a28d92c7a41f7afef007022c2780803c
vicKituku <victorkituku@gmail.com>
2023-01-17 03:58:24 -0800

    print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("(nill)\n");
+			printf("(nil)\n");
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("(nill)\n");
+			printf("(nil)\n");
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("(nill)\n");
+			printf("(nil)\n");
 	}
 }
