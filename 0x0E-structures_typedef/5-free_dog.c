d31eae66bb6f251f553bf0f0df5a4b3bd26bf49e
vicKituku <victorkituku@gmail.com>
2023-01-17 04:52:04 -0800

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog -frees the memory allocated for dog struct
+ * @d: pointer to the memory allocated
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
