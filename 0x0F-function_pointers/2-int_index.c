2ac361d5497d37ad53069df94596de9580ef5a90
vicKituku <victorkituku@gmail.com>
2023-01-19 03:32:19 -0800

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,18 +5,19 @@
  * @array: array to be searched
  * @size: size of the array
  * @cmp: function pointer
+ * Return: index of first match
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (size <= 0)
-	return (-1);	
+	return (-1);
 	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if(cmp(array[i]))
+			if (cmp(array[i]))
 				return (i);
 		}
 	}
