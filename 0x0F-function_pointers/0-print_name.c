e450213cd3df6e5cc2d4b0445776f7036ef2cf4f
vicKituku <victorkituku@gmail.com>
2023-01-19 02:09:57 -0800

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_name -prints a name
+ * @name: string literal
+ * @f: function pointer that returns void
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	f(name);
+}
