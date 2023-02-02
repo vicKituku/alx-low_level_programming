156f1d6775706c27191a797a85f72ea65eed3946
vicKituku <victorkituku@gmail.com>
2023-02-01 23:56:58 -0800

    free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 /**
  * free_listint2 - Frees memory and sets head to NULL
- * @head - Memory location of the first node
+ * @head: Memory location of the first node
  * Return: Nothing
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *temp, *h;
+
 	if (head != NULL)
-		h = *head;
-	while ((temp = h) != NULL)
 	{
-		h = h->next;
-		free(temp);
+		h = *head;
+		while ((temp = h) != NULL)
+		{
+			h = h->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
