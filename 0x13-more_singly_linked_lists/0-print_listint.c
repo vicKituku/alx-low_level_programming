ea308c46b72e4849afd367273914aac419d82888
vicKituku <victorkituku@gmail.com>
2023-02-01 21:16:49 -0800

    print list int + README

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
