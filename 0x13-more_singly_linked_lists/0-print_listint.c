daaf4c0f93d4013ddfb06b5addc6c036a194f93d
vicKituku <victorkituku@gmail.com>
2023-02-01 21:20:28 -0800

    print list int

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * print_listint - Prints all the elements of a list
+ * @h: head of the list
+ * Return: Number of elements in the list
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
