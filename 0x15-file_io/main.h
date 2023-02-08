42c5ecef3927f9fdf160b9891a34fc00ba7dba7d
vicKituku <victorkituku@gmail.com>
2023-02-08 08:14:44 -0800

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define _MAIN_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 #endif
