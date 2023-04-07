a0060a5ce3b9c5dcf8962687954665a0c6d2e112
vicKituku <victorkituku@gmail.com>
2023-04-07 04:26:40 -0700

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
  * Return: The index of the key.
  * Description: Uses the djb2 algorithm.
  */
-unsigned long int get_key_index(const unsigned char *key, unsigned long int table_size)
+unsigned long int key_index(const unsigned char *key, unsigned long int table_size)
 {
     unsigned long int hash_value = hash_djb2(key);
     unsigned long int index = hash_value % table_size;
