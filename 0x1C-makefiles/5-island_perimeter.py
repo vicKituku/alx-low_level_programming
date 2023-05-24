493aaca5896c87365957483c18f239035a14d3f1
vicKituku <victorkituku@gmail.com>
2023-05-24 09:34:00 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Defines a function for calculating the perimeter of an island"""
+
+
 def island_perimeter(grid):
+    """Returns the perimeter of an island
+
+    Args:
+        grid(list): 2-D list
+    Returns:
+        Peremiter of an island
+    """
     len_row = len(grid)
     len_column = len(grid[0])
 
@@ -8,11 +18,10 @@ def island_perimeter(grid):
 
     for x in range(len_row):
         for y in range(len_column):
-            if grid[x][y] ==  1:
+            if grid[x][y] == 1:
                 perimeter += 4
-                if x!=0 and grid[x-1][y] == 1:
+                if x! = 0 and grid[x - 1][y] == 1:
                     connections += 1
-                if y!=0 and grid[x][y-1] == 1:
+                if y! = 0 and grid[x][y - 1] == 1:
                     connections += 1
     return (perimeter - (connections * 2))
-
